8a8245fa241b2b34d1b2da44b2ca1acc2e623a5d
Odyssey <vsakhanda@gmail.com>
2023-09-23 23:01:30 +0300

    new commit

---
+++ src/main/java/com/dreams_2/project/points.java
@@ -0,0 +1,16 @@
+package com.dreams_2.project;
+
+public class points {
+
+    private int id;
+    private String name;
+    private double latitude;
+    private double  longitude;
+    private String description;
+    private String point_adress;
+    private String phone_number;
+    private String link;
+    private String district;
+    private String regions ;
+
+}
