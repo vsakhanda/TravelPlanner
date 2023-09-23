8ca53697af0efbfaf22953ae19ff7e76d7485e75
Odyssey <vsakhanda@gmail.com>
2023-09-23 23:01:30 +0300

    new commit

---
+++ src/main/java/com/dreams_2/project/ProjectDreamApplication.java
@@ -0,0 +1,13 @@
+package com.dreams_2.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDreamApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/dreams_2/project/trips.java
@@ -0,0 +1,12 @@
+package com.dreams_2.project;
+
+public class trips {
+
+    private int id;
+    private String name;
+    private int author;
+    private String description;
+    private String datetime;
+    private boolean is_compleated;
+
+}
